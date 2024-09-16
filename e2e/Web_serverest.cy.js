f375aacebdb54201d8e2d661ed94f493bd3ccbc3
Marcus Vinicius <87437572+marcus1708@users.noreply.github.com>
2024-09-16 11:14:31 -0300

    Update Web_serverest.cy.js

--- e2e/Web_serverest.cy.js
+++ e2e/Web_serverest.cy.js
@@ -2,19 +2,19 @@ describe('Automaçao WEB Serverest', () => {
     beforeEach('Realiza Login no sistema', function() {
         cy.Verif_login()
     });
-    it('Teste Negativo - Usuário já cadastrado', function() {
+    it('Teste Negativo - Usuário já cadastrado anteriormente', function() {
         cy.Login()
     });
-    it('Cadastra Produto', function() {
+    it('Cadastra Produto no sistema', function() {
         cy.Cad_Prod()
     }); 
-    it('Lista Produto', function() {
+    it('Lista Produto cadastrado', function() {
         cy.Lista_Prod()
     });
-    it('Exclui usuário', function() {
+    it('Exclui usuário do sistema', function() {
         cy.Del_User()
     });
-    it('Excluir Produto', function() {
+    it('Excluir Produto do site', function() {
         cy.Del_Prod()
     });
   });

1b4384a391e910d36257c45e8188b8662dfed090
Marcus Vinicius <87437572+marcus1708@users.noreply.github.com>
2024-10-24 17:09:14 -0300

    Update api_serverest.js

--- support/api_serverest.js
+++ support/api_serverest.js
@@ -7,6 +7,15 @@ Cypress.Commands.add('cad_user', (cria_usuario)=>{
             expect(response.status).to.eq(201);
             Cypress.env('id', response.body._id);});
 })
+Cypress.Commands.add('cad_user_existente', (cria_usuario)=>{
+    cy.api({
+        method: 'POST',
+        url: 'https://serverest.dev/usuarios',
+        failOnStatusCode: false,
+        body:cria_usuario
+        }).then((response) => {
+            expect(response.status).to.eq(400);});
+})
 Cypress.Commands.add('login', (login)=>{
     cy.api({
         method: 'POST',
@@ -28,6 +37,18 @@ Cypress.Commands.add('cad_prod', (cria_produto)=>{
             expect(response.status).to.eq(201);
             Cypress.env('id_p', response.body._id);});
 })
+Cypress.Commands.add('cad_prod_existente', (cria_produto)=>{
+    cy.api({
+        method: 'POST',
+        url: 'https://serverest.dev/produtos',
+        body:cria_produto,
+        failOnStatusCode: false,
+        headers: {
+            authorization: Cypress.env('token')
+        }
+        }).then((response) => {
+            expect(response.status).to.eq(400);});
+})
 Cypress.Commands.add('busc_user', ()=>{
     cy.api({
         method: 'GET',
@@ -42,6 +63,14 @@ Cypress.Commands.add('busc_user_id', ()=>{
     }).then((response) => {
         expect(response.status).to.eq(200);});
 })
+Cypress.Commands.add('busc_user_id_inv', ()=>{
+    cy.api({
+        method: 'GET',
+        url: `https://serverest.dev/usuarios/12345`,
+        failOnStatusCode: false
+    }).then((response) => {
+        expect(response.status).to.eq(400);});
+})
 Cypress.Commands.add('busc_prod', ()=>{
     cy.api({
         method: 'GET',
@@ -49,12 +78,20 @@ Cypress.Commands.add('busc_prod', ()=>{
     }).then((response) => {
         expect(response.status).to.eq(200);});
 })
-Cypress.Commands.add('busc_prod_id', (atual_usuario)=>{
+Cypress.Commands.add('busc_prod_id', ()=>{
     cy.api({
         method: 'GET',
         url: `https://serverest.dev/produtos/${Cypress.env('id_p')}`
     }).then((response) => {
         expect(response.status).to.eq(200);});
+})
+Cypress.Commands.add('busc_prod_id_inv', ()=>{
+    cy.api({
+        method: 'GET',
+        failOnStatusCode: false,
+        url: `https://serverest.dev/produtos/123456`
+    }).then((response) => {
+        expect(response.status).to.eq(400);});
 })
 Cypress.Commands.add('atual_user', (atual_usuario)=>{
     cy.api({
@@ -67,6 +104,18 @@ Cypress.Commands.add('atual_user', (atual_usuario)=>{
         }).then((response) => {
             expect(response.status).to.eq(200);});
 })
+Cypress.Commands.add('atual_user_inv', (atual_usuario)=>{
+    cy.api({
+        method: 'PUT',
+        url: `https://serverest.dev/usuarios/12345`,
+        body: atual_usuario,
+        failOnStatusCode: false,
+        headers: {
+            authorization: Cypress.env('token')
+        }
+        }).then((response) => {
+            expect(response.status).to.eq(400);});
+})
 Cypress.Commands.add('atual_prod', (atual_produto)=>{
     cy.api({
         method: 'PUT',
@@ -77,6 +126,18 @@ Cypress.Commands.add('atual_prod', (atual_produto)=>{
         }
         }).then((response) => {
             expect(response.status).to.eq(200);});
+})
+Cypress.Commands.add('atual_prod_inv', (atual_produto)=>{
+    cy.api({
+        method: 'PUT',
+        url: `https://serverest.dev/produtos/12345`,
+        body: atual_produto,
+        failOnStatusCode: false,
+        headers: {
+            authorization: Cypress.env('token')
+        }
+        }).then((response) => {
+            expect(response.status).to.eq(400);});
 })
 Cypress.Commands.add('cad_carr', ()=>{
     cy.api({
@@ -95,6 +156,23 @@ Cypress.Commands.add('cad_carr', ()=>{
             expect(response.status).to.eq(201);
             Cypress.env('id_c', response.body._id);});
 })
+Cypress.Commands.add('cad_carr_inv', ()=>{
+    cy.api({
+        method: 'POST',
+        url: 'https://serverest.dev/carrinhos',
+        failOnStatusCode: false,
+        body:{
+            "produtos":[{   
+                "idProduto": Cypress.env('id_p'),
+                "quantidade": 1
+            }]
+        },
+        headers: {
+            authorization: Cypress.env('token')
+        }
+        }).then((response) => {
+            expect(response.status).to.eq(400);});
+})
 Cypress.Commands.add('list_carr', ()=>{
     cy.api({
         method: 'GET',
@@ -108,6 +186,14 @@ Cypress.Commands.add('carr_id', ()=>{
         url: `https://serverest.dev/carrinhos/${Cypress.env('id_c')}`
     }).then((response) => {
         expect(response.status).to.eq(200);});
+})
+Cypress.Commands.add('carr_id_inv', ()=>{
+    cy.api({
+        method: 'GET',
+        failOnStatusCode: false,
+        url: `https://serverest.dev/carrinhos/123456`
+    }).then((response) => {
+        expect(response.status).to.eq(400);});
 })
 Cypress.Commands.add('del_carr', ()=>{
     cy.api({
@@ -133,6 +219,36 @@ Cypress.Commands.add('del_user', ()=>{
     cy.api({
         method: 'DELETE',
         url: `https://serverest.dev/usuarios/${Cypress.env('id')}`
+    }).then((response) => {
+        expect(response.status).to.eq(200);});
+})
+Cypress.Commands.add('del_carr_inv', ()=>{
+    cy.api({
+        method: 'DELETE',
+        failOnStatusCode: false,
+        url: 'https://serverest.dev/carrinhos/cancelar-compra/',
+        headers: {
+            authorization: Cypress.env('token')
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(401);});
+})
+Cypress.Commands.add('del_prod_inv', ()=>{
+    cy.api({
+        method: 'DELETE',
+        failOnStatusCode: false,
+        url: `https://serverest.dev/produtos/123456`,
+        headers: {
+            authorization: Cypress.env('token')
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(401);});
+})
+Cypress.Commands.add('del_user_inv', ()=>{
+    cy.api({
+        method: 'DELETE',
+        failOnStatusCode: false,
+        url: `https://serverest.dev/usuarios/123456`
     }).then((response) => {
         expect(response.status).to.eq(200);});
 })
